d8b491159ba0f2e72806c8ec14b341d83afe4cef
Marcin Naglik <marcin.naglik@gmail.com>
2014-02-20 23:17:00 +0100

    extracting heroku configuration

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@
 
 exports.index = function(req, res){
 	// need to extract and generate token
-	res.redirect('https://id.heroku.com/oauth/authorize?client_id=feb3855e-4a35-4d99-b5a0-431f7d39c0b1&response_type=code&scope=read&state=ala_ma_kota');
+	res.redirect(process.env.HEROKU_AUTH_URL + '/oauth/authorize?client_id=' + process.env.HEROKU_OAUTH_ID + '&response_type=code&scope=read&state=ala_ma_kota');
 };
--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ exports.list = function(req, res){
 	var postdata = {
 	    grant_type: "authorization_code",
 	    code: req.query.code,
-	    client_secret: '6e24f40b-86f2-43ac-9a50-bb012d92f08a'
+	    client_secret: process.env.HEROKU_OAUTH_SECRET
 	};
 
 	request.post('https://id.heroku.com/oauth/token', {form: postdata}, function (e, r, body) {
