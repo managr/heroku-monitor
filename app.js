648326b1108b23bc298821f03e706842e97256ea
Marcin Naglik <marcin.naglik@gmail.com>
2014-02-21 01:45:09 +0100

    refactoring app to one file

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var env = require('node-env-file');
@@ -37,7 +36,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/heroku/callback', user.list);
+app.get('/heroku/callback', routes.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var request = require('request');
+var Heroku = require('heroku-client');
 
 /*
  * GET home page.
@@ -6,4 +8,29 @@
 exports.index = function(req, res){
 	// need to extract and generate token
 	res.redirect(process.env.HEROKU_AUTH_URL + '/oauth/authorize?client_id=' + process.env.HEROKU_OAUTH_ID + '&response_type=code&scope=read&state=ala_ma_kota');
+};
+
+exports.list = function(req, res){
+
+	var postdata = {
+	    grant_type: "authorization_code",
+	    code: req.query.code,
+	    client_secret: process.env.HEROKU_OAUTH_SECRET
+	};
+
+	request.post('https://id.heroku.com/oauth/token', {form: postdata}, function (e, r, body) {
+		var info = JSON.parse(body);
+		
+		heroku = new Heroku({ token: info.access_token });
+		heroku.apps().list(function (err, apps) {
+			// res.render('index', { token: info.access_token, apps: apps } )
+		});
+
+		heroku.post('/apps/boost-analytics/log-sessions', {"dyno":"web.1","lines":10,"source":"app","tail":true}, function (err, app) {
+			res.render('index', { token: '', apps: [], log_url: app.logplex_url } )
+			// console.log(app.logplex_url);
+		});
+
+	});
+
 };
