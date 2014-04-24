791dbefefa798ce8a20c472b92bc8392f3596c6e
managr <marcin.naglik@gmail.com>
2014-04-24 16:24:02 +0200

    using two spaces

--- app.js
+++ app.js
@@ -9,47 +9,47 @@ var Heroku  = require('heroku-client');
 
 fs.exists(__dirname + '/.env', function (exists) {
   if (exists) {
-		env(__dirname + '/.env');
-	}
+    env(__dirname + '/.env');
+  }
 });
 
 exports.index = function(req, res){
-	// need to extract and generate token
-	res.redirect(process.env.HEROKU_AUTH_URL + '/oauth/authorize?client_id=' + process.env.HEROKU_OAUTH_ID + '&response_type=code&scope=read');
+  // need to extract and generate token
+  res.redirect(process.env.HEROKU_AUTH_URL + '/oauth/authorize?client_id=' + process.env.HEROKU_OAUTH_ID + '&response_type=code&scope=read');
 };
 
 
 exports.list = function(req, res){
-	var postdata = {
-	    grant_type: "authorization_code",
-	    code: req.query.code,
-	    client_secret: process.env.HEROKU_OAUTH_SECRET
-	};
+  var postdata = {
+      grant_type: "authorization_code",
+      code: req.query.code,
+      client_secret: process.env.HEROKU_OAUTH_SECRET
+  };
 
-	request.post('https://id.heroku.com/oauth/token', {form: postdata}, function (e, r, body) {
-		var info = JSON.parse(body);
-		
-		heroku = new Heroku({ token: info.access_token });
-		// we might list apps to pickup correct one
-		// heroku.apps().list(function (err, apps) {
-		// 	// res.render('index', { token: info.access_token, apps: apps } )
-		// });
+  request.post('https://id.heroku.com/oauth/token', {form: postdata}, function (e, r, body) {
+    var info = JSON.parse(body);
+    
+    heroku = new Heroku({ token: info.access_token });
+    // we might list apps to pickup correct one
+    // heroku.apps().list(function (err, apps) {
+    //  // res.render('index', { token: info.access_token, apps: apps } )
+    // });
 
-		heroku.post('/apps/' + process.env.APPLICATION_NAME + '/log-sessions', {"tail":true}, function (err, app) {
+    heroku.post('/apps/' + process.env.APPLICATION_NAME + '/log-sessions', {"tail":true}, function (err, app) {
 
-			stream = request(app.logplex_url);
+      stream = request(app.logplex_url);
 
-			// stream.pipe(res);
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			stream.on('data', function(chunk) {
-				// add transfering that to the socket.io
-      			console.log("Received body data:");
-      			console.log(chunk.toString());
-    			res.write(chunk.toString());
-    		});
+      // stream.pipe(res);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      stream.on('data', function(chunk) {
+        // add transfering that to the socket.io
+            console.log("Received body data:");
+            console.log(chunk.toString());
+          res.write(chunk.toString());
+        });
 
-		});
-	});
+    });
+  });
 };
 
 
